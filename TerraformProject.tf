2697b84a0786a711d09a079987be01b043ecd855
Tollisha Thompson <tee@tollishas-mbp.mynetworksettings.com>
2023-06-08 14:12:19 -0400

    Updates to structure, added Terraform file

---
+++ TerraformProject.tf
@@ -0,0 +1,49 @@
+# Create vpc
+resource "aws_vpc" "mtc_vpc" {
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "dev-vpc"
+  }
+}
+#create Subnet
+resource "aws_subnet" "mtc_public_subnet" {
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-west-2a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
+#Create Internet Gateway
+resource "aws_internet_gateway" "mtc_internet_gateway" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+#Create Route Table to route traffic from subnet to internet gateway
+resource "aws_route_table" "mtc_publc_rt" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "dev_public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id = aws_route_table.mtc_publc_rt.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.mtc_internet_gateway.id
+}  
+
+resource "aws_route_table_association" "mtc_public_assoc" {
+  subnet_id      = aws_subnet.mtc_public_subnet.id
+  route_table_id = aws_route_table.mtc_publc_rt.id
+}
